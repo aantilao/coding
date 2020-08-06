b2f42bba54f1ce53ce32bcb9d59276e7dc7e4030
aantilao <a.antilaoortiz@gmail.com>
2020-08-06 17:48:05 -0400

    ADD: call api example

--- index.js
+++ index.js
@@ -5,4 +5,17 @@ const app = express();
 
 app.use(express.static(path.join(__dirname, 'public')))
 
+console.log('taskete kudasai');
+
 app.listen(5000);
+
+
+const fetch = require("node-fetch");
+const url = 'https://la2.api.riotgames.com/lol/platform/v3/champion-rotations?api_key=RGAPI-4d261a57-f4d6-4c6d-ab28-446734c7efdd';
+
+fetch(url)
+.then(response => response.json())
+.then(data => {
+    console.log(data)
+})
+.catch(err => console.log(err))
