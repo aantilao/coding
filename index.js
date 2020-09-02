f037aa5770422d97a6270f9ef2cee54c7674293f
aantilao <a.antilaoortiz@gmail.com>
2020-09-02 01:01:13 -0400

    update: change port

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 
 const express = require ('express');
-const path = require('path')
 const app = express();
-const { info } = require('console');
 const champion = require('./scripts/champion.js');
-const { rejects } = require('assert');
-const { resolve } = require('path');
 
-//app.use(express.static(path.join(__dirname, 'public')))
+/*
 app.listen(5000, () => {
 	console.log('Server  on Port 5000');
+});*/
+app.set('port', (process.env.PORT || 5000));
+
+app.listen(app.get('port'), function() {
+    console.log('App is running, server is listening on port ', app.get('port'));
 });
 
 app.get('/', (req, res) => {
