0a99123834853eb4da5de4110232a038aab8e6fc
aantilao <a.antilaoortiz@gmail.com>
2020-09-03 17:45:09 -0400

    add: link raiz y item

--- index.js
+++ index.js
@@ -3,17 +3,17 @@ const express = require ('express');
 const app = express();
 const champion = require('./scripts/champion.js');
 
-/*
-app.listen(5000, () => {
-	console.log('Server  on Port 5000');
-});*/
 app.set('port', (process.env.PORT || 5000));
-
 app.listen(app.get('port'), function() {
     console.log('App is running, server is listening on port ', app.get('port'));
 });
 
-app.get('/', (req, res) => {
+
+app.get('/', (req,res) => {
+	res.send('LOL');
+});
+
+app.get('/champions/', (req, res) => {
 	const champ =  algo();
 	champ.then(info=> {
 		res.json(info);
@@ -22,7 +22,10 @@ app.get('/', (req, res) => {
 		res.send(error);
 	});
 });
-
 async function algo(){
 	return champions = await champion.read_champions();	
 }
+
+app.get('/items/', (req, res) => {
+	res.sendFile(__dirname + '/lolsito/parche/img/champion/Akali.png');
+});
